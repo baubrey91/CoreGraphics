0ca9b1b069528ad0690b3af783c0fe132e9c29af
Brandon <brandon@Brandons-MacBook-Pro.local>
2017-03-01 16:41:44 -0800

    3/1/17

--- coreGraphicsTest/ViewController.swift
+++ coreGraphicsTest/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var counterLabel: UILabel!
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var graphView: GraphView!
+    @IBOutlet weak var averageWaterDrunk: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
     
     var isGraphViewShowing = false
     
@@ -31,6 +33,7 @@ class ViewController: UIViewController {
                               duration: 1.0,
                               options: [UIViewAnimationOptions.transitionFlipFromLeft, UIViewAnimationOptions.showHideTransitionViews],
                               completion: nil)
+            setupGraphDisplay()
             
         }
         isGraphViewShowing = !isGraphViewShowing
@@ -60,6 +63,57 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setupGraphDisplay() {
+        
+        //Use 7 days for graph - can use any number,
+        //but labels and sample data are set up for 7 days
+        let noOfDays:Int = 7
+        
+        //1 - replace last day with today's actual data
+        graphView.graphPoints[graphView.graphPoints.count-1] = counterView.counter
+        
+        //2 - indicate that the graph needs to be redrawn
+        graphView.setNeedsDisplay()
+        
+        if let max = graphView.graphPoints.max(){
+            maxLabel.text = "\(max)"
+        }
+        
+        //3 - calculate average from graphPoints
+        let average = graphView.graphPoints.reduce(0, +)
+            / graphView.graphPoints.count
+        averageWaterDrunk.text = "\(average)"
+        
+        //set up labels
+        //day of week labels are set up in storyboard with tags
+        //today is last day of the array need to go backwards
+        
+        //4 - get today's day number
+        let dateFormatter = DateFormatter()
+        let calendar = NSCalendar.current
+        let componentOptions:NSCalendar.Unit = .weekday
+        let components = calendar.dateComponents([.weekday], from: NSDate() as Date)
+        var weekday = components.weekday
+        
+        let days = ["S", "S", "M", "T", "W", "T", "F"]
+        
+        //5 - set up the day name labels with correct day
+        for i in (1...days.count).reversed() {
+            if let labelView = graphView.viewWithTag(i) as? UILabel {
+                if weekday == 0 {
+                    weekday = days.count
+                }
+                weekday = weekday! - 1
+                labelView.text = days[(weekday)!]
+                if weekday! < 1 {
+                    weekday = days.count
+                }
+
+
+            }
+        }
     }
 
 
